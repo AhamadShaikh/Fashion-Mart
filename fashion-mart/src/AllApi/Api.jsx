d6520277c24e6956619a3dcbdbcecbd60ddd7372
AhamadShaikh <shaikhahamadrasul786@gmail.com>
2023-03-31 14:59:30 +0530

    day-3 work is done

--- fashion-mart/src/AllApi/Api.jsx
+++ fashion-mart/src/AllApi/Api.jsx
@@ -1,20 +1,20 @@
 import axios from "axios";
 export const dashboardProduct=()=>{
-    return axios.get(`https://drab-ruby-shrimp-tux.cyclic.app/dashboard-products`)
+    return axios.get(`https://expensive-turtleneck-dove.cyclic.app//dashboard-products`)
 }
 
 export const StaticImage1=()=>{
-    return axios.get(`https://drab-ruby-shrimp-tux.cyclic.app/StaticImage1`)
+    return axios.get(`https://expensive-turtleneck-dove.cyclic.app/StaticImage1`)
 }
 
 export const StaticImage2=()=>{
-    return axios.get(`https://drab-ruby-shrimp-tux.cyclic.app/StaticImage2`)
+    return axios.get(`https://expensive-turtleneck-dove.cyclic.app/StaticImage2`)
 }
 
 export const WomenProducts=(id)=>{
-    return axios.get(`https://drab-ruby-shrimp-tux.cyclic.app/womenswear`)
+    return axios.get(`https://expensive-turtleneck-dove.cyclic.app/womenswear`)
 }
 
 export const MenProducts=(id)=>{
-    return axios.get(`https://drab-ruby-shrimp-tux.cyclic.app/menswear`)
+    return axios.get(`https://expensive-turtleneck-dove.cyclic.app/menswear`)
 }
