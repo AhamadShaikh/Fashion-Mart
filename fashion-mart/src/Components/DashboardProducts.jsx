ee32d315a3168fd6fd927f0e697d35e1a6f2499b
AhamadShaikh <shaikhahamadrasul786@gmail.com>
2023-03-31 23:54:17 +0530

    day-4 work is done

--- fashion-mart/src/Components/DashboardProducts.jsx
+++ fashion-mart/src/Components/DashboardProducts.jsx
@@ -1,16 +1,15 @@
 import { useContext } from 'react'
-import { GridItem, Text, Button } from "@chakra-ui/react"
-import {Link, Navigate} from "react-router-dom"
+import { GridItem, Text, Button, Box, Stack } from "@chakra-ui/react"
+import { Link, Navigate } from "react-router-dom"
 import { getSingleProduct } from '../AllApi/Api';
 import { AuthContext } from '../context/AuthContext';
-const DashboardProducts = ({id, image, name, detail, brandName, totalPrice }) => {
+const DashboardProducts = ({ id, image, name, detail, brandName, totalPrice }) => {
 
-    const {handleSingleProductPage} = useContext(AuthContext);
+    const { handleSingleProductPage } = useContext(AuthContext);
 
-// const handleSingleProductPage=(id)=>{
-//     getSingleProduct(id).then((res)=>console.log(res.data))
-// }
+
     return (
+        <Box>
             <GridItem w='100%' border="1px solid black" p="3" bg="white">
                 <img src={image} width="100%" />
                 <Text as="b" fontSize="20px">{name}</Text>
@@ -19,9 +18,10 @@ const DashboardProducts = ({id, image, name, detail, brandName, totalPrice }) =>
                 <Text as="b">${totalPrice}</Text>
                 <br />
                 <Link to={`/SingleProductPage/${id}`}>
-                <Button color="white" bg="black" _hover="">Shop Now</Button>
+                    <Button color="white" bg="black" _hover="">Shop Now</Button>
                 </Link>
             </GridItem>
+        </Box>
     )
 }
 
--- fashion-mart/src/ProductPages/MenProductPage.jsx
+++ fashion-mart/src/ProductPages/MenProductPage.jsx
@@ -1,29 +1,128 @@
-import {MenProducts} from "../AllApi/Api" 
-import {useState,useEffect} from "react";
-import {Grid} from "@chakra-ui/react";
+import { MenProducts } from "../AllApi/Api"
+import { useState, useEffect, useReducer } from "react";
+import { Grid, Flex, Select, Stack, Text, Button, Box } from "@chakra-ui/react";
 import DashboardProducts from "../Components/DashboardProducts"
 import Navbar from "../Pages/Navbar";
-function MenProductPage() {
-    const [menData, setMenData] = useState([])
+import "./MenProductPage.css"
 
-    const fetchAndUpdateData = () => {
-        MenProducts().then((res) => setMenData(res?.data)).catch((err) => console.log(err))
+const reducer = (state, action) => {
+    switch (action.type) {
+        case "FETCH_LOADING": {
+            return {
+                loading: true,
+                data: [],
+                err: false,
+            }
+        }
+        case "FETCH_SUCCESS": {
+            return {
+                loading: false,
+                data: action.payload,
+                err: false,
+            }
+        }
+        case "FETCH_ERROR": {
+            return {
+                loading: false,
+                data: [],
+                err: true,
+            }
+        }
+        default: {
+            throw new Error(`Invalid Action`)
+        }
     }
-    console.log(menData)
+}
+
+const pageReducer = (pageState, action) => {
+    switch (action.type) {
+        case "INCREMENT": {
+            return pageState + 1
+        }
+        case "DECREMENT": {
+            return pageState - 1
+
+        }
+        default: {
+            throw new Error(`Invalid Action`)
+        }
+    }
+}
+
+
+function MenProductPage() {
+    let initState = {
+        loading: false,
+        data: [],
+        err: false,
+    }
+
+    const [state, dispatch] = useReducer(reducer, initState);
+    const [pageState, pageDispatch] = useReducer(pageReducer, 1)
+    const [order, setOrder] = useState("asc");
+    const [query, setQuery] = useState("")
+
+
+    const fetchAndUpdate = (pageState, order, query) => {
+        console.log(query)
+        dispatch({ type: "FETCH_LOADING" })
+        MenProducts({ order: order, pageState: pageState, query: query })
+            .then((res) => {
+                console.log(res.data)
+                dispatch({ type: "FETCH_SUCCESS", payload: res.data })
+            })
+            .catch(() => {
+                dispatch({ type: "FETCH_ERROR" })
+            })
+    }
 
     useEffect(() => {
-        fetchAndUpdateData();
-    }, [])
+        fetchAndUpdate(pageState, order, query)
+    }, [pageState, order, query])
+
+    const handleSort = (e) => {
+        setOrder(e.target.value)
+    }
+    const handleSearch = (e) => {
+        setQuery(e?.target.value)
+    }
+
     return (
         <div>
-            <Navbar/>
-            <Grid templateColumns='repeat(4, 1fr)' gap={5} bg="blackAlpha.100">
-                {
-                    menData?.map((ele) => (
-                        <DashboardProducts key={ele.id} {...ele} />
-                    ))
-                }
-            </Grid>
+            <div>
+                <Navbar handleSearch={handleSearch} />
+                <Flex>
+                    <Stack mr="5">
+                        <Box border="2px solid black" borderRadius="5px">
+                            <Box border="1px solid black" m={"5"} borderRadius="5px">
+                                <Select w="200px" onChange={handleSort}>
+                                    <option value="asc">price low to high</option>
+                                    <option value="desc">price high to low</option>
+                                </Select>
+                            </Box>
+                            <Box border="1px solid black">
+                                <Text as="b">filter by brands</Text>
+                                <hr />
+                                <Text>Dolce & Gabbana : </Text><input value="Dolce & Gabbana" type="checkbox" onChange={(e) => setQuery(e.target.value)} />
+                                <Text>Cettire : </Text><input value="Cettire" type="checkbox" onChange={(e) => setQuery(e.target.value)} />
+                                <Text>Mytheresa : </Text><input value="Mytheresa" type="checkbox" onChange={(e) => setQuery(e.target.value)} />
+                            </Box>
+                        </Box>
+                    </Stack>
+                    <Grid templateColumns='repeat(3, 1fr)' gap={5} bg="blackAlpha.100">
+                        {
+                            state?.data?.map((ele) => (
+                                <DashboardProducts key={ele.id} {...ele} />
+                            ))
+                        }
+                    </Grid>
+                </Flex>
+            </div>
+            <div className="pageBtn">
+                <button disabled={pageState === 1} onClick={() => pageDispatch({ type: "DECREMENT" })}>PREV</button>
+                <button disabled>{pageState}</button>
+                <button disabled={pageState === 2} onClick={() => pageDispatch({ type: "INCREMENT" })}>NEXT</button>
+            </div>
         </div>
     )
 }
